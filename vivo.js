f73f217b921c145beabeada6fba6ca958b7d7b13
jjalmeidadev <jaime.v.jaime@gmail.com>
2025-02-05 22:51:50 -0300

    ver2

--- vivo.js
+++ vivo.js
@@ -1,23 +1,30 @@
 var config = {
     type: Phaser.AUTO,
+    parent: "phase-example",
     width: 800,
     height: 600,
+    physics: {
+default: 'arcade',
+arcade: {
+    gravity: {y:0},
+    debug:false
+
+}
+    },
 
     scene:{
               preload: preload,
               create: create,
-              update: update
+              update: update,
 
        }
 };
 
 var game = new Phaser.Game(config);
 
-var movimento;
-
 
 function preload() {  
-    this.load.image('pl','assets/person.png');
+    this.load.image("pl",'assets/person.png');
     this.load.image('fun','assets/fundo.png');
 
     
@@ -27,15 +34,32 @@ function create() {
     
     this.add.image(400, 300, 'fun').setOrigin (0.5, 0.5);
 
-    movimento = this.add.image(100, 50, 'pl').setScale(0.3, 0.3)
+    this.pl = this.physics.add.sprite(400, 150, "pl").setScale(0.4,0.4);
 
+    this.cursors = this.input.keyboard.createCursorKeys();
 
-    movimento.setFlip(true, false);
+   
     
 }
 
 function update() {  
-
+    if(this.cursors.left.isDown){
+        console.log("esquerda");
+        this.pl.setVelocityX(-100);
+      }else if(this.cursors.right.isDown){
+        console.log("direita");
+        this.pl.setVelocityX(100);
+      }else if(this.cursors.up.isDown){
+        console.log("subir");
+        this.pl.setVelocityY(-100);
+      }else if(this.cursors.down.isDown){
+        console.log("descer");
+        this.pl.setVelocityY(100);
+      }else{
+        this.pl.setVelocityX(0);
+        this.pl.setVelocityY(0);
+      }
+      }
    
     
-              }
+              
